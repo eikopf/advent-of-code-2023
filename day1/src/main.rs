c8a864ee3baf150ce0721527127ff2fce5bb9720
Oliver Wooding <oliverwooding@icloud.com>
2023-12-03 15:33:00 +0000

    solved day 1 question 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,34 +2,69 @@ use std::io;
 
 use nom::{
     IResult, 
-    character::complete::{alpha0, digit1}, 
+    character::complete::{alpha0, anychar}, 
     sequence::delimited, 
     multi::{separated_list0, many1}, 
-    character::complete::{u8, alpha1, one_of}, 
+    character::complete::{alpha1, one_of}, 
     Parser, 
-    error::VerboseError,
-    combinator::opt
+    combinator::{opt, map_res, map}, 
+    branch::alt, bytes::complete::tag
 };
 
-/// Parses the input into a Vec<u8>, according to the question 1 specification.
-fn parse_literal_digits(source: &str) -> IResult<&str, Vec<u8>> {
+/// Parses a single digit from the source input.
+fn single_digit(source: &str) -> IResult<&str, u8> {
+    let mut parser = map_res(one_of("123456789"), |c| String::from(c).parse::<u8>());
+    parser.parse(source)
+}
+
+/// Parses a single named digit from the given source.
+fn named_digit(source: &str) -> IResult<&str, u8> {
+    let mut parser = alt((
+        tag("one"),
+        tag("two"),
+        tag("three"),
+        tag("four"),
+        tag("five"),
+        tag("six"),
+        tag("seven"),
+        tag("eight"),
+        tag("nine"),
+    ));
+
+    parser.parse(source).map(|(tail, name)| (tail, match name {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => unreachable!(),
+    }))
+}
+
+/// Parses a line into a Vec<u8>, according to the question 1 specification.
+fn literal_digits(source: &str) -> IResult<&str, Vec<u8>> {
     let mut parser = delimited(
         opt(alpha0), 
         separated_list0(
             alpha1,
-            many1(one_of("0123456789")).map(|v| 
-                v.into_iter().map(|c| 
-                    u8::<&str, VerboseError<&str>>(
-                        String::from(c)
-                            .as_str())
-                            .unwrap()
-                            .1
-                )
-            )
+            many1(single_digit),
         ), 
         opt(alpha0), 
     );
     parser.parse(source).map(|(tail, data)| (tail, data.into_iter().flatten().collect()))
+}
+
+/// Parses a line into a Vec<u8>, according to the question 2 specification.
+fn literal_and_named_digits(source: &str) -> IResult<&str, Vec<u8>> {
+    let mut parser = many1(
+        alt((named_digit, single_digit, map(anychar, |_| 0)))
+    );
+
+    parser.parse(source).map(|(tail, v)| (tail, v.into_iter().filter(|&x| x != 0).collect()))
 }
 
 /// Computes the answer to question 1 by taking input from stdin
@@ -39,7 +74,23 @@ fn get_q1_result() -> anyhow::Result<usize> {
 
     for line in lines {
         let input = line.unwrap();
-        let digits = parse_literal_digits(&input).unwrap().1;
+        let digits = literal_digits(&input).unwrap().1;
+        eprintln!("{:?}, {:?}", input, digits);
+        let first = *digits.first().unwrap() as usize;
+        let last = *digits.last().unwrap() as usize;
+        acc += (first * 10) + last;
+    }
+
+    Ok(acc)
+}
+
+fn get_q2_result() -> anyhow::Result<usize> {
+    let lines = io::stdin().lines();
+    let mut acc: usize = 0;
+
+    for line in lines {
+        let input = line.unwrap();
+        let digits = literal_and_named_digits(&input).unwrap().1;
         eprintln!("{:?}, {:?}", input, digits);
         let first = *digits.first().unwrap() as usize;
         let last = *digits.last().unwrap() as usize;
@@ -50,6 +101,6 @@ fn get_q1_result() -> anyhow::Result<usize> {
 }
 
 fn main() {
-    let res = get_q1_result().unwrap();
+    let res = get_q2_result().unwrap();
     println!("{:?}", res);
 }
