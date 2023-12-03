b1453a8640f00e0239cbf7923ebfd76f70a3b537
Oliver Wooding <oliverwooding@icloud.com>
2023-12-03 16:52:34 +0000

    added day3 stuff

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,17 @@
+use nom::IResult;
+
+/// Represents the elements which can appear in an engine schematic.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum SchematicItem {
+    Dot,
+    Number({value: usize, length: u8}),
+    Symbol,
+}
+
+fn parse_schematic_line(source: &str) -> IResult<&str, Vec<SchematicItem>> {
+    todo!()
+}
+
 fn main() {
     println!("Hello, world!");
 }
