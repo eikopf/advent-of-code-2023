ffd9f85594b6e9358567c59abfdcc61c42bc0ff5
Oliver Wooding <oliverwooding@icloud.com>
2023-12-06 10:41:59 +0000

    completed day 6 question 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,11 @@
 use nom::{
-    IResult, 
-    sequence::{preceded, Tuple}, 
-    bytes::complete::tag, 
-    character::complete::{multispace1, u16, newline}, 
-    Parser, multi::separated_list1, combinator::map_res
+    bytes::complete::tag,
+    character::complete::{digit1, multispace1, newline, u16},
+    combinator::map_res,
+    error::Error,
+    multi::separated_list1,
+    sequence::{preceded, Tuple},
+    Finish, IResult, Parser,
 };
 
 /// Represents a single race (column) from the source data.
@@ -18,31 +20,59 @@ struct Race {
 /// Parses the entire input into a list of races.
 fn parse_inputs(source: &str) -> IResult<&str, Vec<Race>> {
     let times = preceded(
-        tag("Time:").and(multispace1), 
-        separated_list1(
-            multispace1, 
-            map_res(u16, |x| usize::try_from(x)),
-        )
+        tag("Time:").and(multispace1),
+        separated_list1(multispace1, map_res(u16, |x| usize::try_from(x))),
     );
 
     let distances = preceded(
-        tag("Distance:").and(multispace1), 
-        separated_list1(
-            multispace1, 
-            map_res(u16, |x| usize::try_from(x)),
-        )
+        tag("Distance:").and(multispace1),
+        separated_list1(multispace1, map_res(u16, |x| usize::try_from(x))),
     );
 
     (times, newline, distances)
         .parse(source)
-        .map(|(tail, (times, _, distances))| 
-            (tail, times
-                .into_iter()
-                .zip(distances.into_iter())
-                .map(|(duration, record_distance)| Race { duration, record_distance })
-                .collect()
+        .map(|(tail, (times, _, distances))| {
+            (
+                tail,
+                times
+                    .into_iter()
+                    .zip(distances.into_iter())
+                    .map(|(duration, record_distance)| Race {
+                        duration,
+                        record_distance,
+                    })
+                    .collect(),
             )
-        )
+        })
+}
+
+/// Parses the entire input into a single race.
+fn parse_single_race(source: &str) -> IResult<&str, Race> {
+    let time = preceded(
+        tag("Time:").and(multispace1),
+        map_res(separated_list1(multispace1, digit1), |digits| {
+            digits.join("").parse::<usize>()
+        }),
+    );
+
+    let distance = preceded(
+        tag("Distance:").and(multispace1),
+        map_res(separated_list1(multispace1, digit1), |digits| {
+            digits.join("").parse::<usize>()
+        }),
+    );
+
+    (time, newline, distance)
+        .parse(source)
+        .map(|(tail, (duration, _, record_distance))| {
+            (
+                tail,
+                Race {
+                    duration,
+                    record_distance,
+                },
+            )
+        })
 }
 
 /// For some reason this function boundary makes the compiler
@@ -50,28 +80,57 @@ fn parse_inputs(source: &str) -> IResult<&str, Vec<Race>> {
 /// it.
 ///
 /// Maybe there are some implied lifetimes here?
-fn get_races() -> Vec<Race> {
+fn get_races() -> anyhow::Result<Vec<Race>> {
     let source = aoc::read_stdin_to_string();
-    let (_, races) = parse_inputs(&source).unwrap();
-    races
+    match parse_inputs(&source).finish() {
+        Ok((_, races)) => Ok(races),
+        Err(Error { input, code }) => Err(Error {
+            input: input.to_string(),
+            code,
+        }
+        .into()),
+    }
+}
+
+fn get_race() -> anyhow::Result<Race> {
+    let source = aoc::read_stdin_to_string();
+    match parse_single_race(&source).finish() {
+        Ok((_, race)) => Ok(race),
+        Err(Error { input, code }) => Err(Error {
+            input: input.to_string(),
+            code,
+        }
+        .into()),
+    }
 }
 
 fn get_q1_result() -> anyhow::Result<usize> {
-    let races = get_races();
+    let races = get_races()?;
     eprintln!("{:?}", races);
 
     Ok(races
         .into_iter()
-        .map(|Race { duration, record_distance }| 
-            (0..=duration)
+        .map(
+            |Race {
+                 duration,
+                 record_distance,
+             }| {
+                (0..=duration)
                     .zip((0..=duration).rev())
-                    .zip(std::iter::repeat(record_distance)))
+                    .zip(std::iter::repeat(record_distance))
+            },
+        )
         .map(|iter| iter.filter(|((a, b), dist)| (a * b) > *dist).count())
         .product())
 }
 
 fn get_q2_result() -> anyhow::Result<usize> {
-    todo!()
+    let race = get_race()?;
+
+    Ok((0..=(race.duration))
+        .zip((0..=(race.duration)).rev())
+        .filter(|(a, b)| a * b > race.record_distance)
+        .count())
 }
 
 fn main() {
@@ -79,7 +138,7 @@ fn main() {
     let res = match cli.question {
         aoc::Question::One => get_q1_result(),
         aoc::Question::Two => get_q2_result(),
-    }.unwrap();
+    }
+    .unwrap();
     println!("{}", res);
-
 }
