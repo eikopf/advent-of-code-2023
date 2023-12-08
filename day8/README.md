e3da9aab5ce9cfb9f6e112061e0e42ea0303d9da
Oliver Wooding <oliverwooding@icloud.com>
2023-12-08 13:10:46 +0000

    updates to day 8

---
+++ day8/README.md
@@ -0,0 +1,10 @@
+# Day 8
+## Question 1
+I was slightly spooked by the idea that I would have to implement a graph in rust, but luckily this question basically just requires a hashmap. Traversing the graph is pretty trivial, so there's nothing clever to do here.
+
+## Question 2
+I originally tried to write a bruteforce solution, but it occurs to me that that would take ages and be basically pointless. Instead, consider that we can run each traversal up until the point that they loop back to their initial values.
+
+> The assumption here is that each node is a component of a (relatively) short cycle, such that we only have to concern ourselves with a single loop through each of them. I'm also going to assume that each cycle only has one solution, otherwise this calculation is significantly more painful.
+
+This gives us a list of cycle lengths, and we also want to know the indices of the terminal nodes in each cycle. Call the cycle lengths $L_i$ and the terminal indices $t_i$ for the cycle $i$; then in particular we know that the set of solutions for cycle $i$ is $$\text{Sol}_i=\{n\in\mathbb{N}_0:t_i\equiv n\text{ mod}L_i\}.$$
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -23,6 +23,8 @@ struct Node([char; 3]);
 struct Network(HashMap<Node, (Node, Node)>);
 
 impl Network {
+    /// Traverses the network according to the given path and
+    /// returns the number of steps to get from AAA to ZZZ.
     fn traverse(&self, path: &mut impl Iterator<Item = Side>) -> usize {
         let mut count = 0;
         let mut current_node = Node(['A', 'A', 'A']);
@@ -39,7 +41,39 @@ impl Network {
                 panic!("ran out of directions");
             }
 
-            if current_node == Node(['Z', 'Z', 'Z']) { break; }
+            if current_node == Node(['Z', 'Z', 'Z']) {
+                break;
+            }
+        }
+
+        count
+    }
+
+    /// Traverses the network in lockstep, starting from all nodes which
+    /// end with A, and terminating when the path produces a set of nodes
+    /// which all end with Z; then the function returns the length of the
+    /// traversal.
+    fn ghost_traverse(&self, path: &mut impl Iterator<Item = Side>) -> usize {
+        let mut count = 0;
+        let mut current_nodes: Vec<Node> = self
+            .0
+            .keys()
+            .filter(|&node| node.0[2] == 'A')
+            .map(|&node| node)
+            .collect();
+
+        loop {
+            if let Some(side) = path.next() {
+                count += 1;
+                current_nodes = current_nodes.into_iter().map(|node| match side {
+                    Side::Left => self.0.get(&node).unwrap().0,
+                    Side::Right => self.0.get(&node).unwrap().1,
+                }).collect();
+            } else {
+                panic!("ran out of directions");
+            }
+            
+            if current_nodes.iter().filter(|node| node.0[2] == 'Z').count() == current_nodes.len() { break; }
         }
 
         count
@@ -98,7 +132,9 @@ fn get_q1_result() -> anyhow::Result<usize> {
 }
 
 fn get_q2_result() -> anyhow::Result<usize> {
-    todo!()
+    let (path, network) = parse_input_data()?;
+    let mut path_loop = path.into_iter().cycle();
+    Ok(network.ghost_traverse(&mut path_loop))
 }
 
 fn main() {
@@ -106,6 +142,7 @@ fn main() {
     let res = match cli.question {
         aoc::Question::One => get_q1_result(),
         aoc::Question::Two => get_q2_result(),
-    }.unwrap();
+    }
+    .unwrap();
     println!("{}", res);
 }
